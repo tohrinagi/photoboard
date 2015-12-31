af254ff713ff2bb0f2378954198c906647a52a1a
tohrinagi <tohrinagi@gmail.com>
2015-12-31 17:59:59 +0900

    Initial Commit

---
+++ photoboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoboard
+//
+//  Created by tohrinagi on 2015/12/31.
+//  Copyright © 2015年 tohrinagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
